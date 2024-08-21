ee6d793f11723d559720d9c1b9d3752c874ed82a
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-08-21 16:52:12 +0530

    Review comment updated

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,66 +10,6 @@ char arr_with_ret_4[1] = {'L'};
 char arr_with_ret_5[2] = {'M','N'};
 char arr_with_ret_6[1] = {'R'};
 
-
-// Test cases for getSoundexCode function
-TEST(SoundexTest_ret_1, GetSoundexCode)
-{
-    for (int i=0;i<4;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_1[i]), '1');
-    }
-}
-
-TEST(SoundexTest_ret_2, GetSoundexCode)
-{
-    for (int i=0;i<8;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_2[i]), '2');
-    }
-}
-
-TEST(SoundexTest_ret_3, GetSoundexCode)
-{
-    for (int i=0;i<2;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_3[i]), '3');
-    }
-}
-
-TEST(SoundexTest_ret_4, GetSoundexCode)
-{
-    for (int i=0;i<1;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_4[i]), '4');
-    }
-    
-}
-
-TEST(SoundexTest_ret_5, GetSoundexCode)
-{
-    for (int i=0;i<2;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_5[i]), '5');
-    }
-    
-}
-
-TEST(SoundexTest_ret_6, GetSoundexCode)
-{
-    for (int i=0;i<1;i++)
-    {
-        EXPECT_EQ(getSoundexCode(arr_with_ret_6[i]), '6');
-    }
-    
-}
-
-TEST(SoundexTest_default, GetSoundexCode)
-{
-    EXPECT_EQ(getSoundexCode('A'), '0');
-    EXPECT_EQ(getSoundexCode('E'), '0');
-    EXPECT_EQ(getSoundexCode('Y'), '0');
-}
-
 TEST(SoundexTest_numerical, GetSoundexCode)
 {
     EXPECT_EQ(getSoundexCode('1'), '0'); // numerical value
